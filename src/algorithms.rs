9d5c8bffca4094abd9921294ed00576f831da177
Ciklon <ciklon@mailbox.org>
2025-11-04 12:56:33 +0100

    Initial commit

---
+++ src/algorithms.rs
@@ -0,0 +1,42 @@
+mod insertionsort;
+
+/// The different sorting algorithms
+#[derive(Clone, Copy, clap::ValueEnum)]
+pub enum Algorithm {
+    /// The algorithm used by the rust std library
+    Std,
+    /// Insertion sort
+    Insertion,
+    /// Binary Insertion sort
+    BinaryInsertion,
+}
+
+impl std::fmt::Display for Algorithm {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str(match self {
+            Algorithm::Std => "Standard library sort",
+            Algorithm::Insertion => "Insertion sort",
+            Algorithm::BinaryInsertion => "Binary insertion sort",
+        })
+    }
+}
+
+impl Algorithm {
+    /// The sort function
+    pub fn sorter<T: Ord>(self) -> fn(&mut [T]) {
+        match self {
+            Algorithm::Std => <[T]>::sort,
+            Algorithm::Insertion => insertionsort::insertion_sort,
+            Algorithm::BinaryInsertion => insertionsort::binary_insertion_sort,
+        }
+    }
+
+    /// Return whether the sort is stable
+    pub fn is_stable(self) -> bool {
+        match self {
+            Algorithm::Std => true,
+            Algorithm::Insertion => true,
+            Algorithm::BinaryInsertion => true,
+        }
+    }
+}
---
+++ src/algorithms/insertionsort.rs
@@ -0,0 +1,46 @@
+// TODO: consider working with pointers/unsafe? of course there are a lot of in bounds checks here
+
+/// Sort slice using insertion sort, assuming that `slice[0..partition]` is already in order
+pub fn insertion_sort_with_partition<T: Ord>(slice: &mut [T], partition_point: usize) {
+    assert!((0..slice.len()).contains(&partition_point));
+
+    for i in partition_point..slice.len() {
+        for j in (0..i).rev() {
+            if slice[j + 1] < slice[j] {
+                // TODO: swapping is easiest, otherwise I'd have to work with unsafe I think
+                slice.swap(j + 1, j);
+            } else {
+                break;
+            }
+        }
+    }
+}
+
+/// Sort the slice using insertion sort
+pub fn insertion_sort<T: Ord>(slice: &mut [T]) {
+    insertion_sort_with_partition(slice, 1);
+}
+
+/// Sort slice using binary insertion sort, assuming that `slice[0..partition]` is already in order
+pub fn binary_insertion_sort_with_partition<T: Ord>(slice: &mut [T], partition_point: usize) {
+    assert!((0..slice.len()).contains(&partition_point));
+
+    for i in partition_point..slice.len() {
+        let mut j = slice[0..i]
+            .binary_search(&slice[i])
+            .unwrap_or_else(|index| index);
+        // Necessary for stability, TODO: is this correct w.r. c++ impl?
+        while j < i && slice[j] == slice[i] {
+            j += 1;
+        }
+
+        for p in (j..i).rev() {
+            slice.swap(p, p + 1);
+        }
+    }
+}
+
+/// Sort the slice using insertion sort
+pub fn binary_insertion_sort<T: Ord>(slice: &mut [T]) {
+    binary_insertion_sort_with_partition(slice, 1);
+}
