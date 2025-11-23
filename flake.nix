6efb62056ec63f410959022f39a2d0937c5ba4d9
Ciklon <ciklon@mailbox.org>
2025-11-23 18:58:10 +0100

    Changing setup to allow running under MIRI

--- flake.nix
+++ flake.nix
@@ -3,20 +3,43 @@
     naersk.url = "github:nix-community/naersk/master";
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, utils, naersk }:
-    utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      utils,
+      naersk,
+      rust-overlay,
+    }:
+    utils.lib.eachDefaultSystem (
+      system:
       let
-        pkgs = import nixpkgs { inherit system; };
-        naersk-lib = pkgs.callPackage naersk { };
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ (import rust-overlay) ];
+        };
+        toolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+        naersk-lib = pkgs.callPackage naersk {
+          cargo = toolchain;
+          rustc = toolchain;
+        };
       in
       {
         defaultPackage = naersk-lib.buildPackage ./.;
-        devShell = with pkgs; mkShell {
-          buildInputs = [ cargo rustc rustfmt pre-commit rustPackages.clippy rust-analyzer ];
-          RUST_SRC_PATH = rustPlatform.rustLibSrc;
-        };
+        devShell =
+          with pkgs;
+          mkShell {
+            nativeBuildInputs = [
+              toolchain
+              rust-analyzer
+            ];
+          };
       }
     );
 }
