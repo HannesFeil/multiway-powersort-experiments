d4a78984365202466b26ea6df91de31c3c738ca0
HannesFeil <hannesfeil@gmx.de>
2025-11-23 17:25:51 +0100

    Preventing UB on panicking while merging

--- src/algorithms/merging.rs
+++ src/algorithms/merging.rs
@@ -16,7 +16,10 @@ impl<T> BufGuard<T> for Vec<T> {
     }
 }
 
+/// Specifies ways to merge two adjacent runs in a slice, given a buffer
 pub trait MergingMethod {
+    /// Merge the two sorted runs `0..split_point` and `split_point..slice.len()`, potentially
+    /// using `buffer`.
     fn merge<T: Ord, B: BufGuard<T>>(
         slice: &mut [T],
         split_point: usize,
@@ -24,6 +27,10 @@ pub trait MergingMethod {
     );
 }
 
+/// A [`MergingMethod`] implementation via a simple merging procedure
+///
+/// The `buffer` given in [`Self::merge`] has to have at least the same
+/// size as the `slice`.
 #[derive(Debug, Clone, Copy)]
 pub struct CopyBoth;
 
@@ -42,19 +49,8 @@ impl MergingMethod for CopyBoth {
             "Split points needs to be in bounds"
         );
 
-        // SAFETY: We know slice and buffer are distinct and just checked then length of buffer.
-        // Since T and MaybeUninit<T> are guaranteed to have the same size, alignment and abi
-        // this copy is sound.
-        unsafe {
-            std::ptr::copy_nonoverlapping(
-                slice.as_ptr(),
-                buffer.as_mut_ptr() as *mut T,
-                slice.len(),
-            );
-        }
-
-        let mut output = slice.as_mut_ptr();
-        let (left, right) = buffer.split_at(split_point);
+        let mut output = buffer.as_mut_ptr();
+        let (left, right) = slice.split_at(split_point);
         let std::ops::Range {
             start: mut left_start,
             end: left_end,
@@ -64,30 +60,57 @@ impl MergingMethod for CopyBoth {
             end: right_end,
         } = right.as_ptr_range();
 
-        // SAFETY: All pointers are kept in bounds of their respective range and each element from
-        // buffer ends up being copied to slice exactly once
+        // NOTE: We copy after the merging as opposed to before, to prevent inconsistent
+        // state which could occur when panicking on merging into slice
+
+        // SAFETY: All pointers from slice are kept in bounds of their respective range.
+        // Since it is assumed that slice.len() <= buffer.len() and in total slice.len()
+        // elements are written into buffer one by one, these accesses are guaranteed to be
+        // in bounds as well. The writing is valid since MaybeUninit<T> has the same layout,
+        // size and ABI as as T and elements in [T] are guaranteed to be laid out sequentially
+        // in memory (see https://doc.rust-lang.org/reference/type-layout.html#slice-layout)).
+        //
+        // Additionally each element is written into buffer exactly once,
+        // so that buffer ends up as a permutation of slice.
         unsafe {
+            // Repeatedly copy the smaller element of both runs into the buffer
             while left_start != left_end && right_start != right_end {
-                if (*left_start).assume_init_ref() < (*right_start).assume_init_ref() {
-                    output.copy_from_nonoverlapping(left_start as *const T, 1);
+                if *left_start < *right_start {
+                    output
+                        .copy_from_nonoverlapping(left_start as *const std::mem::MaybeUninit<T>, 1);
                     left_start = left_start.add(1);
                 } else {
-                    output.copy_from_nonoverlapping(right_start as *const T, 1);
+                    output.copy_from_nonoverlapping(
+                        right_start as *const std::mem::MaybeUninit<T>,
+                        1,
+                    );
                     right_start = right_start.add(1);
                 }
 
                 output = output.add(1);
             }
+
+            // Copy the rest of the remaining run into the buffer
             while left_start < left_end {
-                output.copy_from_nonoverlapping(left_start as *const T, 1);
+                output.copy_from_nonoverlapping(left_start as *const std::mem::MaybeUninit<T>, 1);
                 left_start = left_start.add(1);
                 output = output.add(1);
             }
             while right_start < right_end {
-                output.copy_from_nonoverlapping(right_start as *const T, 1);
+                output.copy_from_nonoverlapping(right_start as *const std::mem::MaybeUninit<T>, 1);
                 right_start = right_start.add(1);
                 output = output.add(1);
             }
+        }
+
+        // SAFETY: Since buffer now contains a permutation of slice, we can safely copy it over to
+        // slice, again regarding the same layout invariant for T and MaybeUninit<T>. (see above)
+        unsafe {
+            std::ptr::copy_nonoverlapping(
+                buffer.as_ptr() as *mut T,
+                slice.as_mut_ptr(),
+                slice.len(),
+            );
         }
     }
 }
